a3324c6b0279d9cc83ef109aa566a64de36bcaf4
E2r7hN07Fl47 <43937145+E2r7hN07Fl47@users.noreply.github.com>
2020-08-11 02:14:26 +0300

    Moved project files to new dir, added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='udbworker',
+      version='0.1',
+      description='(not)Universal Database Worker',
+      packages=['udbworker'],
+      author_email='E2r7hN07Fl47@protonmail.com',
+      zip_safe=False)
