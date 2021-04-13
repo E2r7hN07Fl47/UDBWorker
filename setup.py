0182863d7ebb2fdf2c7270ddb616ee27ccf6b549
E2r7hN07Fl47 <43937145+E2r7hN07Fl47@users.noreply.github.com>
2021-04-14 02:07:50 +0300

    Minor bug fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='udbworker',
-      version='0.3.0',
+      version='0.3.1',
       description='(not)Universal Database Worker',
       packages=['udbworker'],
       author_email='E2r7hN07Fl47@protonmail.com',
